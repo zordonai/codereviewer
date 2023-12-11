db37aa5bcf9834289e226f5117f4ac20e73af70c
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-11 16:58:46 -0300

    chore: update build

--- dist/index.js
+++ dist/index.js
@@ -33053,7 +33053,7 @@ exports.getInputs = getInputs;
 /***/ }),
 
 /***/ 6144:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+/***/ (function(module, exports, __nccwpck_require__) {
 
 "use strict";
 
@@ -33094,24 +33094,41 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+var fs_1 = __nccwpck_require__(7147);
 var get_inputs_1 = __nccwpck_require__(1464);
 var octokit_1 = __nccwpck_require__(2167);
 var palm_1 = __nccwpck_require__(5257);
 var inputs = (0, get_inputs_1.getInputs)();
 var octokit = (0, octokit_1.getOctokit)(inputs.githubToken);
 var palm = (0, palm_1.getPalmAPI)(inputs.palmApiKey);
-var start = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var prDetails;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, (0, octokit_1.getPRDetails)(octokit)];
-            case 1:
-                prDetails = _a.sent();
-                console.log({ prDetails: prDetails });
-                return [2 /*return*/];
-        }
+function run() {
+    var _a;
+    return __awaiter(this, void 0, void 0, function () {
+        var event, prDetails, diff;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    event = JSON.parse((0, fs_1.readFileSync)((_a = process.env.GITHUB_EVENT_PATH) !== null && _a !== void 0 ? _a : "", "utf8"));
+                    return [4 /*yield*/, (0, octokit_1.getPRDetails)(octokit)];
+                case 1:
+                    prDetails = _b.sent();
+                    return [4 /*yield*/, (0, octokit_1.getPRDiff)({
+                            octokit: octokit,
+                            owner: prDetails.owner,
+                            repo: prDetails.repo,
+                            pull_number: prDetails.pull_number,
+                        })];
+                case 2:
+                    diff = _b.sent();
+                    console.log({ prDetails: prDetails, diff: diff, event: event });
+                    return [2 /*return*/];
+            }
+        });
     });
-}); };
+}
+module.exports = {
+    run: run,
+};
 
 
 /***/ }),
@@ -33241,15 +33258,84 @@ exports.getPRDetails = getPRDetails;
 
 /***/ }),
 
+/***/ 7978:
+/***/ (function(__unused_webpack_module, exports) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getPRDiff = void 0;
+var getPRDiff = function (_a) {
+    var octokit = _a.octokit, owner = _a.owner, repo = _a.repo, pull_number = _a.pull_number;
+    return __awaiter(void 0, void 0, void 0, function () {
+        var response;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0: return [4 /*yield*/, octokit.rest.pulls.get({
+                        owner: owner,
+                        repo: repo,
+                        pull_number: pull_number,
+                        mediaType: { format: "diff" },
+                    })];
+                case 1:
+                    response = _b.sent();
+                    return [2 /*return*/, response.data];
+            }
+        });
+    });
+};
+exports.getPRDiff = getPRDiff;
+
+
+/***/ }),
+
 /***/ 9426:
 /***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.getPRDetails = void 0;
+exports.getPRDiff = exports.getPRDetails = void 0;
 var get_pr_details_1 = __nccwpck_require__(4158);
 Object.defineProperty(exports, "getPRDetails", ({ enumerable: true, get: function () { return get_pr_details_1.getPRDetails; } }));
+var get_pr_diff_1 = __nccwpck_require__(7978);
+Object.defineProperty(exports, "getPRDiff", ({ enumerable: true, get: function () { return get_pr_diff_1.getPRDiff; } }));
 
 
 /***/ }),
