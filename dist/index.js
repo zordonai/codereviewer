a1e79d55753ca1525256c876a7da2d3a280410d6
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-13 16:49:55 -0300

    chore: add code review with methods ask and generateText using PaLM

--- dist/index.js
+++ dist/index.js
@@ -33957,17 +33957,77 @@ __exportStar(__nccwpck_require__(2048), exports);
 /***/ }),
 
 /***/ 6267:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.analyzeCode = void 0;
 var create_prompt_1 = __nccwpck_require__(9220);
 var analyzeCode = function (_a) {
     var palm = _a.palm, diff = _a.diff, title = _a.title, description = _a.description;
-    var prompt = (0, create_prompt_1.createPrompt)(diff, title, description);
-    console.log({ prompt: prompt });
+    return __awaiter(void 0, void 0, void 0, function () {
+        var prompt, result1, result2;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    prompt = (0, create_prompt_1.createPrompt)(diff, title, description);
+                    return [4 /*yield*/, palm.generateText(prompt, {
+                            temperature: 0.5,
+                            candidate_count: 1,
+                        })];
+                case 1:
+                    result1 = _b.sent();
+                    return [4 /*yield*/, palm.generateText(prompt, {
+                            temperature: 0.5,
+                            candidate_count: 1,
+                        })];
+                case 2:
+                    result2 = _b.sent();
+                    console.log({
+                        result1String: JSON.stringify(result1),
+                        result2String: JSON.stringify(result2),
+                    });
+                    return [2 /*return*/];
+            }
+        });
+    });
 };
 exports.analyzeCode = analyzeCode;
 
--- src/palm/utils/analyze-code/index.ts
+++ src/palm/utils/analyze-code/index.ts
@@ -1,12 +1,24 @@
 import { IAnalyzeCode } from "./interface";
 import { createPrompt } from "../create-prompt";
 
-export const analyzeCode = ({
+export const analyzeCode = async ({
   palm,
   diff,
   title,
   description,
 }: IAnalyzeCode) => {
   const prompt = createPrompt(diff, title, description);
-  console.log({ prompt });
+  const result1 = await palm.generateText(prompt, {
+    temperature: 0.5,
+    candidate_count: 1,
+  });
+  const result2 = await palm.generateText(prompt, {
+    temperature: 0.5,
+    candidate_count: 1,
+  });
+
+  console.log({
+    result1String: JSON.stringify(result1),
+    result2String: JSON.stringify(result2),
+  });
 };
