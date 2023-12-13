1166e081042e43b6ff468469172369a24ca22486
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-13 16:36:02 -0300

    chore: update build

--- dist/index.js
+++ dist/index.js
@@ -33957,59 +33957,17 @@ __exportStar(__nccwpck_require__(2048), exports);
 /***/ }),
 
 /***/ 6267:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 "use strict";
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.analyzeCode = void 0;
 var create_prompt_1 = __nccwpck_require__(9220);
 var analyzeCode = function (_a) {
     var palm = _a.palm, diff = _a.diff, title = _a.title, description = _a.description;
-    return __awaiter(void 0, void 0, void 0, function () {
-        var prompt;
-        return __generator(this, function (_b) {
-            prompt = (0, create_prompt_1.createPrompt)(diff, title, description);
-            console.log({ prompt: prompt });
-            return [2 /*return*/];
-        });
-    });
+    var prompt = (0, create_prompt_1.createPrompt)(diff, title, description);
+    console.log({ prompt: prompt });
 };
 exports.analyzeCode = analyzeCode;
 
@@ -34017,61 +33975,21 @@ exports.analyzeCode = analyzeCode;
 /***/ }),
 
 /***/ 9220:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 "use strict";
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.createPrompt = void 0;
 var get_files_changes_1 = __nccwpck_require__(5422);
-var createPrompt = function (diff, title, description) { return __awaiter(void 0, void 0, void 0, function () {
-    var filesChanges, prompt, _i, filesChanges_1, _a, file, content, changes;
-    return __generator(this, function (_b) {
-        filesChanges = (0, get_files_changes_1.getFilesChanges)(diff);
-        prompt = "Please perform a code review considering the following aspects:\n1. Consider the file extension\n2. Code structure and organization\n3. Naming conventions and formatting\n4. Clarity and readability of the code\n5. Efficiency and optimization\n6. Logic and correct functioning of the code\n7. Error handling and exception handling\n8. Code comments and documentation\n9. Security best practices\n10. Consider performance using BigO notation\n\nExtra Instructions:\n1. Provide the response in following JSON format: [{ \"file\": <file_name>, \"reviews\": [{\"lineNumber\": <line_number>, \"reviewComment\": \"<review comment>\"}] }]\n2. Do not give positive comments or compliments\n3. Provide comments as an Staff Engineer\n4. Provide comments and suggestions ONLY if there is something to improve\n5. Write the comment in GitHub Markdown format\n6. Use the given description only for the overall context and only comment the code\n7. Make clear performance improvements, better understanding and explain why\n8. If have suggestions, consider to give examples in how to do\n9. IMPORTANT: NEVER suggest adding comments or descriptions to the code\n10. IMPORTANT\u00B2: BEWARE of duplicate comments and limit duplication to a maximum of 2\n\nPull request title: ".concat(title, "\nPull request description:\n\n---\n").concat(description, "\n---\n\nFiles:\n\n");
-        for (_i = 0, filesChanges_1 = filesChanges; _i < filesChanges_1.length; _i++) {
-            _a = filesChanges_1[_i], file = _a.file, content = _a.content, changes = _a.changes;
-            prompt += "---\nfile_name: ".concat(file, "\n\n```diff\n").concat(content, "\n").concat(changes, "\n```\n---\n");
-        }
-        return [2 /*return*/];
-    });
-}); };
+var createPrompt = function (diff, title, description) {
+    var filesChanges = (0, get_files_changes_1.getFilesChanges)(diff);
+    var prompt = "Please perform a code review considering the following aspects:\n1. Consider the file extension\n2. Code structure and organization\n3. Naming conventions and formatting\n4. Clarity and readability of the code\n5. Efficiency and optimization\n6. Logic and correct functioning of the code\n7. Error handling and exception handling\n8. Code comments and documentation\n9. Security best practices\n10. Consider performance using BigO notation\n\nExtra Instructions:\n1. Provide the response in following JSON format: [{ \"file\": <file_name>, \"reviews\": [{\"lineNumber\": <line_number>, \"reviewComment\": \"<review comment>\"}] }]\n2. Do not give positive comments or compliments\n3. Provide comments as an Staff Engineer\n4. Provide comments and suggestions ONLY if there is something to improve\n5. Write the comment in GitHub Markdown format\n6. Use the given description only for the overall context and only comment the code\n7. Make clear performance improvements, better understanding and explain why\n8. If have suggestions, consider to give examples in how to do\n9. IMPORTANT: NEVER suggest adding comments or descriptions to the code\n10. IMPORTANT\u00B2: BEWARE of duplicate comments and limit duplication to a maximum of 2\n\nPull request title: ".concat(title, "\nPull request description:\n\n---\n").concat(description, "\n---\n\nFiles:\n\n");
+    for (var _i = 0, filesChanges_1 = filesChanges; _i < filesChanges_1.length; _i++) {
+        var _a = filesChanges_1[_i], file = _a.file, content = _a.content, changes = _a.changes;
+        prompt += "---\nfile_name: ".concat(file, "\n\n```diff\n").concat(content, "\n").concat(changes, "\n```\n---\n");
+    }
+};
 exports.createPrompt = createPrompt;
 
 
